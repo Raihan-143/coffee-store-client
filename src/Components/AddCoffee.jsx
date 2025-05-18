147daf2bc5646650bd1810449467977ab32a1fb7
Raihan-143 <raihan172hasan@gmail.com>
2025-05-18 12:22:22 +0600

    added to done post method

--- src/Components/AddCoffee.jsx
+++ src/Components/AddCoffee.jsx
@@ -3,7 +3,23 @@ import React from 'react';
 const AddCoffee = () => {
     const handleAddCoffee=e=>{
         e.preventDefault();
-        const form=e.targer;
+        const form=e.target;
+        const formData=new FormData(form);
+        const newCoffee=Object.fromEntries(formData.entries())
+        console.log(newCoffee);
+
+        //send coffee data to the db
+        fetch('http://localhost:3000/coffees',{
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(newCoffee)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log('after adding coffee to the db',data);
+        })
     }
     return (
         <div className='p-24  bg-[#F4F3F0] shadow'>
@@ -20,8 +36,8 @@ const AddCoffee = () => {
                         <input type="text" name='name' className="input w-full" placeholder="Coffee Name" />
                     </fieldset>
                     <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-                        <label className="label">Chef</label>
-                        <input type="text" name='chef' className="input w-full" placeholder="Enter coffee chef" />
+                        <label className="label">Quantity</label>
+                        <input type="text" name='quantity' className="input w-full" placeholder="Enter coffee quantity" />
                     </fieldset>
                     <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
                         <label className="label">Supplier</label>
